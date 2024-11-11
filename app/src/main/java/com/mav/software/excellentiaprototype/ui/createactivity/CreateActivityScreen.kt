c1ba1044682a7bebf5995d5d0122989fbcabc978
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-11 20:47:54 -0300

    savepoint

--- app/src/main/java/com/mav/software/excellentiaprototype/ui/createactivity/CreateActivityScreen.kt
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/createactivity/CreateActivityScreen.kt
@@ -1,11 +1,21 @@
 package com.mav.software.excellentiaprototype.ui.createactivity
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.mav.software.excellentiaprototype.R
 import com.mav.software.excellentiaprototype.ui.createactivity.components.HorizontalActivityPagerExample
 import com.mav.software.excellentiaprototype.ui.shared.components.ScaffoldExample
 
@@ -24,6 +34,38 @@ fun CreateActivityScreen(
             onClick = {}
         ) {
             Text(text = "Seleccionar")
+        }
+    }
+}
+
+@Composable
+fun CreateActivityScreen2(
+    modifier: Modifier = Modifier
+) {
+    var text by remember { mutableStateOf("") }
+    ScaffoldExample(
+        modifier = modifier,
+        title = "Crear actividad"
+    ) {
+        Column {
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.image_example),
+                    contentDescription = "ImageMultipleChoice"
+                )
+                FilledTonalButton(
+                    onClick = {}
+                ) {
+                    Text(text = "Galería")
+                }
+            }
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text(text = "Consigna") }
+            )
         }
     }
 }
