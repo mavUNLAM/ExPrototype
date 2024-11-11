87b37e2fdefccea7555002cf67953fbd8927cf91
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-11 06:47:21 -0300

    savepoint

--- app/src/main/java/com/mav/software/excellentiaprototype/ui/createactivity/components/HorizontalActivityPager.kt
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/createactivity/components/HorizontalActivityPager.kt
@@ -3,6 +3,7 @@ package com.mav.software.excellentiaprototype.ui.createactivity.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,7 +15,6 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -34,7 +34,7 @@ fun HorizontalActivityPagerExample(
     val coroutineScope = rememberCoroutineScope()
 
 
-    Box(modifier = modifier) {
+    Column(modifier = modifier) {
         HorizontalPager(
             modifier = Modifier,
             state = pagerState,
@@ -59,7 +59,6 @@ fun HorizontalActivityPagerExample(
             Modifier
                 .wrapContentHeight()
                 .fillMaxWidth()
-                .align(Alignment.BottomCenter)
                 .padding(bottom = 8.dp),
             horizontalArrangement = Arrangement.Center
         ) {
