e95f316a7efca3c0d361270241db71a5c9ce4702
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-12 00:41:49 -0300

    savepoint

--- app/src/main/java/com/mav/software/excellentiaprototype/ui/doactivity/DoActivityScreen.kt
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/doactivity/DoActivityScreen.kt
@@ -2,10 +2,16 @@ package com.mav.software.excellentiaprototype.ui.doactivity
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,7 +29,7 @@ fun DoActivityScreen(
 
     ScaffoldExample(
         modifier = modifier,
-        title = "Seleccionar materias"
+        title = "Configuración de actividades"
     ) {
         Column(
             modifier = Modifier
@@ -31,15 +37,37 @@ fun DoActivityScreen(
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
+            Row(
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier
+                        .weight(0.75f)
+                        .padding(start = 10.dp, end = 10.dp),
+                    text = "Apareceran las actividades de las siguientes materias",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    minLines = 2
+                )
+                Icon(
+                    Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Flechita abajo",
+                    modifier = Modifier.padding(end = 10.dp)
+                )
+            }
             Text(
-                modifier = Modifier.padding(10.dp),
+                modifier = Modifier.padding(top = 10.dp, start = 10.dp, end = 10.dp),
                 text = "Materias activas",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold
             )
             SubjectList()
             Text(
-                modifier = Modifier.padding(10.dp),
+                modifier = Modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp),
                 text = "Materias pasadas",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold
