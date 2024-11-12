31c28c6a1337f5b94de543bcefcd23ebc624c476
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-12 00:19:40 -0300

    savepoint

--- app/src/main/java/com/mav/software/excellentiaprototype/ui/doactivity/DoActivityScreen.kt
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/doactivity/DoActivityScreen.kt
@@ -1,10 +1,16 @@
 package com.mav.software.excellentiaprototype.ui.doactivity
 
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mav.software.excellentiaprototype.ui.doactivity.components.SubjectList
 import com.mav.software.excellentiaprototype.ui.doactivity.components.defaultFinishedList
 import com.mav.software.excellentiaprototype.ui.shared.components.ScaffoldExample
@@ -19,13 +25,30 @@ fun DoActivityScreen(
         modifier = modifier,
         title = "Seleccionar materias"
     ) {
-        Text(text = "Materias activas")
-        SubjectList()
-        HorizontalDivider()
-        Text(text = "Materias pasadas")
-        SubjectList(
-            list = defaultFinishedList
-        )
+        Column(
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(
+                modifier = Modifier.padding(10.dp),
+                text = "Materias activas",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            SubjectList()
+            Text(
+                modifier = Modifier.padding(10.dp),
+                text = "Materias pasadas",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            SubjectList(
+                list = defaultFinishedList
+            )
+        }
+
     }
 }
 
