ba7b6dfab0f9deff39a98da8cc1124e4778befd9
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-11 21:41:15 -0300

    savepoint

--- app/src/main/java/com/mav/software/excellentiaprototype/ui/createactivity/CreateActivityScreen.kt
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/createactivity/CreateActivityScreen.kt
@@ -3,6 +3,9 @@ package com.mav.software.excellentiaprototype.ui.createactivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mav.software.excellentiaprototype.R
 import com.mav.software.excellentiaprototype.ui.createactivity.components.HorizontalActivityPagerExample
 import com.mav.software.excellentiaprototype.ui.shared.components.ScaffoldExample
@@ -41,7 +45,7 @@ fun CreateActivityScreen(
 @Preview(showBackground = true)
 @Composable
 fun CreateActivityScreen2(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier.fillMaxSize()
 ) {
     var questionText by remember { mutableStateOf("") }
     var option1Text by remember { mutableStateOf("") }
@@ -53,41 +57,54 @@ fun CreateActivityScreen2(
         modifier = modifier,
         title = "Crear actividad"
     ) {
-        Column {
-            Box(
-                contentAlignment = Alignment.Center
+        val commonModifier = Modifier
+            .align(Alignment.CenterHorizontally)
+            .padding(horizontal = 20.dp)
+            .fillMaxWidth()
+
+        Box(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.image_example),
+                contentDescription = "ImageMultipleChoice"
+            )
+            FilledTonalButton(
+                onClick = {}
             ) {
-                Image(
-                    painter = painterResource(R.drawable.image_example),
-                    contentDescription = "ImageMultipleChoice"
-                )
-                FilledTonalButton(
-                    onClick = {}
-                ) {
-                    Text(text = "Galería")
-                }
+                Text(text = "Galería")
+            }
             }
             OutlinedTextField(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth(),
                 value = questionText,
                 onValueChange = { questionText = it },
                 label = { Text(text = "Pregunta") }
             )
             OutlinedTextField(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
                 value = option1Text,
                 onValueChange = { option1Text = it },
                 label = { Text(text = "Opción 1") }
             )
             OutlinedTextField(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
                 value = option2Text,
                 onValueChange = { option2Text = it },
                 label = { Text(text = "Opción 2") }
             )
             OutlinedTextField(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
                 value = option3Text,
                 onValueChange = { option3Text = it },
                 label = { Text(text = "Opción 3") }
             )
             OutlinedTextField(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
                 value = option4Text,
                 onValueChange = { option4Text = it },
                 label = { Text(text = "Opción 4") }
@@ -99,5 +116,5 @@ fun CreateActivityScreen2(
                 Text(text = "Preview")
             }
         }
-    }
+
 }
