572713de94a9fd3f472f8243f0025d7cd01da263
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-15 03:34:33 -0300

    save point

--- app/src/main/java/com/mav/software/excellentiaprototype/ui/login/components/LogInPopup.kt
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/login/components/LogInPopup.kt
@@ -2,18 +2,16 @@ package com.mav.software.excellentiaprototype.ui.login.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -24,101 +22,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
-@Composable
-fun LogInPopup(
-    modifier: Modifier = Modifier
-) {
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun loginScreen() {
-
-    // State variables to store user input
-    val userName = remember {
-        mutableStateOf("")
-    }
-    val userPassword = remember {
-        mutableStateOf("")
-    }
-
-    // Column to arrange UI elements vertically
-    Column(
-        modifier = Modifier
-            .fillMaxHeight()
-            .padding(40.dp)
-    ) {
-
-        // Welcome message
-        Text(
-            text = "Hello,\nWelcome to the login page", fontSize = 25.sp, color = Color.Blue,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 50.dp, 0.dp, 0.dp)
-        )
-
-        // Username input field
-        OutlinedTextField(
-            value = userName.value, onValueChange = {
-                userName.value = it
-            },
-            leadingIcon = {
-                Icon(Icons.Default.Person, contentDescription = "person")
-            },
-            label = {
-                Text(text = "username")
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 20.dp, 0.dp, 0.dp)
-        )
-
-        // Password input field
-        OutlinedTextField(
-            value = userPassword.value, onValueChange = {
-                userPassword.value = it
-            },
-            leadingIcon = {
-                Icon(Icons.Default.Info, contentDescription = "password")
-            },
-            label = {
-                Text(text = "password")
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 20.dp, 0.dp, 0.dp),
-            visualTransformation = PasswordVisualTransformation()
-        )
-
-        // Login button
-        OutlinedButton(
-            onClick = { },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 25.dp, 0.dp, 0.dp)
-        ) {
-            Text(
-                text = "Login",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(5.dp),
-                textAlign = TextAlign.Center,
-                fontSize = 20.sp
-            )
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
@@ -128,11 +38,23 @@ fun LoginFields(
 ) {
     var user by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var isUserError by remember { mutableStateOf(false) }
+    var isPasswordError by remember { mutableStateOf(false) }
+    var userErrorText by remember { mutableStateOf("") }
+    var passwordErrorText by remember { mutableStateOf("") }
 
     Column(modifier = modifier) {
         OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 5.dp )
+                .fillMaxWidth(),
+            isError = isUserError,
             value = user,
-            onValueChange = { user = it},
+            onValueChange = {
+                user = it
+                isUserError = false
+                userErrorText = ""
+            },
             label = {
                 Text(text = "Usuario")
             },
@@ -149,9 +71,24 @@ fun LoginFields(
             }
         )
 
+        Text(
+            modifier = Modifier.padding(horizontal = 10.dp),
+            text = userErrorText,
+            color = MaterialTheme.colorScheme.error,
+            fontSize = 12.sp
+        )
+
         OutlinedTextField(
+            modifier = Modifier
+                .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 5.dp )
+                .fillMaxWidth(),
+            isError = isPasswordError,
             value = password,
-            onValueChange = { password = it},
+            onValueChange = {
+                password = it
+                isPasswordError = false
+                passwordErrorText = ""
+            },
             label = {
                 Text(text = "Contraseña")
             },
@@ -168,11 +105,31 @@ fun LoginFields(
             }
         )
 
+        Text(
+            modifier = Modifier.padding(horizontal = 10.dp),
+            text = passwordErrorText,
+            color = MaterialTheme.colorScheme.error,
+            fontSize = 12.sp
+        )
+
         Spacer(modifier = Modifier.height(10.dp))
 
         TextButton(onClick = onForgotPasswordClick, modifier = Modifier.align(Alignment.End)) {
             Text(text = "¿Olvidaste tu contraseña?")
         }
+
+        LoginFooter(
+            onSignInClick = {
+                if (user.isEmpty() || user != "estudiante" && user != "profesor") {
+                    isUserError = true
+                    userErrorText = "* Usuario incorrecto"
+                }
+                if (password.isEmpty()) {
+                    isPasswordError = true
+                    passwordErrorText = "* Contraseña incorrecta"
+                }
+            }
+        )
     }
 }
 
@@ -187,11 +144,16 @@ fun LoginFooter(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = onSignInClick, modifier = Modifier.fillMaxWidth()) {
+        Button(
+            onClick = onSignInClick,
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .fillMaxWidth()
+        ) {
             Text(text = "Iniciar sesión")
         }
         TextButton(onClick = onSignUpClick) {
-            Text(text = "¿No tienes una cuenta? Registrate aquí.")
+            Text(text = "¿No tienes una cuenta? Registrate aquí")
         }
     }
 }
