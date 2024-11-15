eed8a5274445da124be3e6477d608bf99e1eee00
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-15 01:15:18 -0300

    temp code from san google

---
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/login/components/LogInPopup.kt
@@ -0,0 +1,189 @@
+package com.mav.software.excellentiaprototype.ui.login.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LogInPopup(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun loginScreen() {
+
+    // State variables to store user input
+    val userName = remember {
+        mutableStateOf("")
+    }
+    val userPassword = remember {
+        mutableStateOf("")
+    }
+
+    // Column to arrange UI elements vertically
+    Column(modifier = Modifier
+        .fillMaxHeight()
+        .padding(40.dp)) {
+
+        // Welcome message
+        Text(text = "Hello,\nWelcome to the login page", fontSize = 25.sp, color = Color.Blue,
+            modifier = Modifier.fillMaxWidth().padding(0.dp, 50.dp, 0.dp, 0.dp)
+        )
+
+        // Username input field
+        OutlinedTextField(value = userName.value, onValueChange = {
+            userName.value = it
+        },
+            leadingIcon = {
+                Icon(Icons.Default.Person, contentDescription = "person")
+            },
+            label = {
+                Text(text = "username")
+            },
+            modifier = Modifier.fillMaxWidth().padding(0.dp, 20.dp, 0.dp, 0.dp)
+        )
+
+        // Password input field
+        OutlinedTextField(value = userPassword.value, onValueChange = {
+            userPassword.value = it
+        },
+            leadingIcon = {
+                Icon(Icons.Default.Info, contentDescription = "password")
+            },
+            label = {
+                Text(text = "password")
+            },
+            modifier = Modifier.fillMaxWidth().padding(0.dp, 20.dp, 0.dp, 0.dp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        // Login button
+        OutlinedButton(onClick = { },
+            modifier = Modifier.fillMaxWidth().padding(0.dp, 25.dp, 0.dp, 0.dp)) {
+            Text(text = "Login",
+                modifier = Modifier.fillMaxWidth().padding(5.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun LoginFields(
+    email: String, password: String,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onForgotPasswordClick: () -> Unit
+) {
+    Column {
+        TextField(
+            value = email,
+            label = "Email",
+            placeholder = "Enter your email address",
+            onValueChange = onEmailChange,
+            leadingIcon = {
+                Icon(Icons.Default.Email, contentDescription = "Email")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            )
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        TextField(
+            value = password,
+            label = "Password",
+            placeholder = "Enter your password",
+            onValueChange = onPasswordChange,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Go
+            ),
+            leadingIcon = {
+                Icon(Icons.Default.Lock, contentDescription = "Password")
+            }
+        )
+
+        TextButton(onClick = onForgotPasswordClick, modifier = Modifier.align(Alignment.End)) {
+            Text(text = "Forgot Password?")
+        }
+    }
+}
+
+@Composable
+fun LoginFooter(
+    onSignInClick: () -> Unit,
+    onSignUpClick: () -> Unit
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = onSignInClick, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Sign In")
+        }
+        TextButton(onClick = onSignUpClick) {
+            Text(text = "Don't have an account, click here")
+        }
+    }
+}
+
+@Composable
+fun TextField(
+    value: String,
+    label: String,
+    placeholder: String,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(text = label)
+        },
+        placeholder = {
+            Text(text = placeholder)
+        },
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon
+    )
+}
