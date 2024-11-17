e85ba57200294df245215c5d340abea010216a73
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-17 10:55:49 -0300

    modificado scaffold example

--- app/src/main/java/com/mav/software/excellentiaprototype/ui/shared/components/Scaffold.kt
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/shared/components/Scaffold.kt
@@ -26,14 +26,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import com.mav.software.excellentiaprototype.ui.theme.ExcellentiaPrototypeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,8 +40,6 @@ fun ScaffoldExample(
     title: String = "Top app bar",
     content: @Composable ColumnScope.() -> Unit = {}
 ) {
-    var presses by remember { mutableIntStateOf(0) }
-
     Scaffold(
         topBar = {
             TopAppBar(
@@ -52,7 +48,8 @@ fun ScaffoldExample(
                         Icons.Default.Menu,
                         contentDescription = "Menu",
                         modifier = Modifier
-                            .padding(horizontal = 5.dp)
+                            .padding(horizontal = 5.dp),
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer
                     )
                 },
                 navigationIcon = {
@@ -60,7 +57,8 @@ fun ScaffoldExample(
                         Icons.AutoMirrored.Default.ArrowBack,
                         contentDescription = "Back",
                         modifier = Modifier
-                            .padding(horizontal = 5.dp)
+                            .padding(horizontal = 5.dp),
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer
                     )
                 },
                 colors = topAppBarColors(
@@ -68,20 +66,23 @@ fun ScaffoldExample(
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 title = {
-                    Text(title)
+                    Text(
+                        text = title,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
                 }
             )
         },
         bottomBar = {
             BottomAppBar(
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
             ) {
                 BottomBar(modifier = Modifier.fillMaxWidth())
             }
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = { presses++ }) {
+            FloatingActionButton(onClick = { }) {
                 Icon(Icons.Default.Add, contentDescription = "Add")
             }
         }
@@ -95,6 +96,17 @@ fun ScaffoldExample(
             content()
         }
     }
+}
+
+@PreviewLightDark
+@Composable
+private fun ScaffoldExamplePreview() {
+    ExcellentiaPrototypeTheme {
+        ScaffoldExample(
+            title = "Ejemplo"
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
