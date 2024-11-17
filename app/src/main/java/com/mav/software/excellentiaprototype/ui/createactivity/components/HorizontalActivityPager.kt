2b23bb7c9fa541cbc201cc9b779fe64bfaba4327
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-17 11:42:40 -0300

    modificado horizontal activity pager example, update

--- app/src/main/java/com/mav/software/excellentiaprototype/ui/createactivity/components/HorizontalActivityPager.kt
+++ app/src/main/java/com/mav/software/excellentiaprototype/ui/createactivity/components/HorizontalActivityPager.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -64,7 +65,9 @@ fun HorizontalActivityPagerExample(
             ) {
                 Text(
                     text = "Multiple choice",
-                    modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp)
+                    modifier = Modifier.padding(horizontal = 5.dp, vertical = 2.dp),
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.onBackground
                 )
                 MultipleChoiceExample()
             }
